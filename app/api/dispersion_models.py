1a5b0204628f73eebd08791b796bf3f6f1998345
johannalg1642 <johannalg1642@gmail.com>
2023-04-28 01:32:16 -0700

    cambios finales

---
+++ app/api/dispersion_models.py
@@ -0,0 +1,82 @@
+from app.api.utils import get_nk_from_dielectric_fuction
+
+
+class DispersionModels(object):
+    """This class implements the main dispersion models"""
+
+    def __init__(self, parameters):
+
+        self.electron_charge = 1.602176634e-19  # in coulombs
+        self.electron_mass = 9.10938356e-31  # in kilograms
+        self.permitivitty_vacuum = 8.8541878128e-12  # in  F/m 
+        self.h = 6.582119569e-16 #eV⋅s
+        self.parameters = parameters
+        # Lorenz :[electrons_per_unit, omega_0, omega, damping_coefficient]
+        # Drude [omega_p, epsilon_infinito, omega, damping_coefficient]
+        # Sellmeier [A, B, C, wavelength] 
+
+    def get_lorenz_model(self):
+        """
+        This method:
+        *Calculates the epsilon_1 and epsilon_2 values of the dielectric function using the lorenz model
+        *Receives a list of all parameters entered by the user
+        * Return epsilon_1 and epsilon_2
+        """
+        a = (self.electron_charge**2 * (self.parameters[0]/self.h)) / (
+            self.permitivitty_vacuum * self.electron_mass
+        )
+        b = (self.parameters[1]/self.h) ** 2 - (self.parameters[2]/self.h) ** 2
+        c = (self.parameters[3]/self.h) * (self.parameters[2]/self.h)
+
+        epsilon_1 = 1 + (a * ((b) / (b**2 + c**2)))
+        epsilon_2 = a * ((c) / (b**2 + c**2))
+        
+        
+        n_k = get_nk_from_dielectric_fuction(epsilon_1, epsilon_2)
+        return n_k
+
+    def get_drude_model(self):
+        """
+        This method:
+        *Calculates the epsilon_1 and epsilon_2 values of the dielectric function using the drude model
+        *Receives a list of all parameters entered by the user
+        * Return epsilon_1 and epsilon_2
+        """
+        a = (self.parameters[0]/self.h) **2
+        b = a * (self.parameters[3]/self.h)
+        c = (self.parameters[2]/self.h)**2 + (self.parameters[3]/self.h)**2
+        
+        epsilon_1 = self.parameters[1] - ((a) / (c))
+        epsilon_2 = (b) / ((self.parameters[2]/self.h) * c)
+        n_k = get_nk_from_dielectric_fuction(epsilon_1, epsilon_2)
+        return n_k
+
+    def get_sellmeier_model(self):
+        """
+        This method:
+        *Calculates the epsilon_1 and epsilon_2 values of the dielectric function using the Sellmeier model
+        *Receives a list of all parameters entered by the user
+        * Return epsilon_1 and epsilon_2
+        """
+        a = self.parameters[1] * self.parameters[2] ** 2
+        b = self.parameters[2] ** 2 - self.parameters[3] ** 2
+
+        epsilon_1 = self.parameters[0] + (a / b)
+        epsilon_2 = 0
+        n_k = get_nk_from_dielectric_fuction(epsilon_1, epsilon_2)
+        return n_k
+
+    def get_cauchy_model(self):
+        """
+        This method:
+        *Calculates the epsilon_1 and epsilon_2 values of the dielectric function using the cauchy model
+        *Receives a list of all parameters entered by the user
+        * Return n and k
+        """
+        a = self.parameters[1] / self.parameters[3] ** 2
+        b = self.parameters[2] / self.parameters[3]
+
+        n = self.parameters[0] + a + b
+        k = 0
+        return complex(n, k)
+
